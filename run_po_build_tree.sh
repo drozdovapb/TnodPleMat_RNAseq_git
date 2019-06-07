36c08cffde9252375cc1f0e9397c96e59039e330
Polina Drozdova <drozdovapb@gmail.com>
2019-06-07 12:55:58 +0800

    described the phylogenetic analysis

--- run_po_build_tree.sh
+++ run_po_build_tree.sh
@@ -1,2 +1,47 @@
+## get the sequences
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/dibothriocephalus_latus/PRJEB1206/dibothriocephalus_latus.PRJEB1206.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/clonorchis_sinensis/PRJNA386618/clonorchis_sinensis.PRJNA386618.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/echinococcus_canadensis/PRJEB8992/echinococcus_canadensis.PRJEB8992.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/echinococcus_granulosus/PRJEB121/echinococcus_granulosus.PRJEB121.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/echinococcus_multilocularis/PRJEB122/echinococcus_multilocularis.PRJEB122.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/hydatigera_taeniaeformis/PRJEB534/hydatigera_taeniaeformis.PRJEB534.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/hymenolepis_diminuta/PRJEB507/hymenolepis_diminuta.PRJEB507.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/hymenolepis_microstoma/PRJEB124/hymenolepis_microstoma.PRJEB124.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/hymenolepis_nana/PRJEB508/hymenolepis_nana.PRJEB508.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/mesocestoides_corti/PRJEB510/mesocestoides_corti.PRJEB510.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/schistocephalus_solidus/PRJEB527/schistocephalus_solidus.PRJEB527.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/spirometra_erinaceieuropaei/PRJEB1202/spirometra_erinaceieuropaei.PRJEB1202.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/taenia_asiatica/PRJEB532/taenia_asiatica.PRJEB532.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/taenia_multiceps/PRJNA307624/taenia_multiceps.PRJNA307624.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/taenia_saginata/PRJNA71493/taenia_saginata.PRJNA71493.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/taenia_solium/PRJNA170813/taenia_solium.PRJNA170813.WBPS13.protein.fa.gz
+#outgroup
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/clonorchis_sinensis/PRJNA386618/clonorchis_sinensis.PRJNA386618.WBPS13.protein.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS13/species/schistosoma_mansoni/PRJEA36577/schistosoma_mansoni.PRJEA36577.WBPS13.protein.fa.gz
+
 ## run proteinortho
 $appdir/proteinortho_v6.0b/proteinortho6.pl -singles -project=Tnod -cpus=6 -nograph *fa
+## this file is processed with `sort_po_table.R` => and we have lists of names by group, as well as by organism. We'll need both.
+
+## now extract the sequences from all fasta files (by species so far)
+for file in *fa; do xargs faidx $file < $file.names.txt | fasta_formatter >tree/$file.oto.fa ; done
+cd ../tree/
+cat *fa >all.fasta
+## and now extract for each protein group one-by-one
+for names in family*names.txt; do xargs faidx all.fasta < $names > $names.fasta ; done
+
+## align each protein group
+for multifasta in family*fasta; do mafft --auto $multifasta >$multifasta.aln; done
+## gblocks filtering
+ls *aln* >gblocks.paths.txt
+$appdir/Gblocks_0.91b/Gblocks gblocks.paths.txt 
+
+## a bit more formatting
+for file in family*.names.txt.fasta.aln-gb; do fasta_formatter -i $file >$file.singleline.fa; done
+paste -d="" *singleline.fa | sed -e 's/=>//g' | sed -e 's/=//g' | sed -e 's/*//g' | sed -e's/ //g' >cestodes_concatenated_aln.fasta
+## may require a bit of manual cleanup if iqtree throws an error about some symbols, but hopefully not
+
+## finally build the tree! 
+$appdir/iqtree-1.6.10-Linux/bin/iqtree -s cestodes_concatenated_aln.fasta -abayes -alrt 1000
+
+## then use ggtree to plot it and inkscape to make it pretty (`build_tree.R`)
--- sort_po_table.R
+++ sort_po_table.R
@@ -1,55 +1,62 @@
 options(stringsAsFactors = F)
 
+## read the proteinortho table
 tbl <- read.delim("data/Tnod.proteinortho", na.strings = "*")
 
-### now unique transcripts
-#how many unique transcripts? 
-unique_transcripts <- unique(sub(".p.*", "", tnod$TnodPleMat_final.fasta.transdecoder.fa))
-length(unique_transcripts) #transcripts
-#16999 transcripts
-presentTnod <- tbl[! is.na(tbl$TnodPleMat_final.fasta.transdecoder.fa), ] #32k
-View(colSums(!is.na(presentTnod[,2:21])))
+## total number of protein group in each species
+colSums(!is.na(tbl[,2:21]))
 
-expTnod <- presentTnod[grepl(",", presentTnod$TnodPleMat_final.fasta.transdecoder.fa), 1:4]
-
-
+## species-specific proteins
 onlyone <- tbl[tbl$X..Species == 1,]
+## how many species-specific groups for each
+colSums(!is.na(onlyone[,4:21]))
 
-#this is for unique in each
-colSums(!is.na(onlyone[,2:21]))
-
-#unique proteins #are these the same as unique groups? 
+## unique proteins #are these the same as unique groups? 
 tnod <- tbl[tbl$X..Species == 1 & !is.na(tbl$TnodPleMat_final.fasta.transdecoder.fa), ]
 length(tnod$TnodPleMat_final.fasta.transdecoder.fa) #protein groups
 sum(tnod$Genes) #protein groups
 #yeap! #cool!
 
+### now unique transcripts (just in case)
+## how many unique transcripts? 
+unique_transcripts <- unique(sub(".p.*", "", tbl$TnodPleMat_final.fasta.transdecoder.fa))
+length(unique_transcripts) ## transcripts (of course more than proteins)
 
+## present in T. nodulosus
+presentTnod <- tbl[! is.na(tbl$TnodPleMat_final.fasta.transdecoder.fa), ] #32k
+## shared with T. nodulosus
+colSums(!is.na(presentTnod[,2:21]))
+
+## Present in two species only
 onlytwo <- tbl[tbl$X..Species == 2,] #19k
+## One of these species in T. nodulosus
 twowithTnod <- onlytwo[! is.na(onlytwo$TnodPleMat_final.fasta.transdecoder.fa), ]
 View(colSums(!is.na(twowithTnod[,2:23])))
 
 ##and now add uniqueness information to the transcripts
-annotation <- read.delim("./TnodPleMat_AnnotationTable.txt")
-annotation$Unique <- ifelse(annotation$Sequence.Name %in% unique_transcripts, "unique for T. nodulosus", "")
+#annotation <- read.delim("./TnodPleMat_AnnotationTable.txt")
+#annotation$Unique <- ifelse(annotation$Sequence.Name %in% unique_transcripts, "unique for T. nodulosus", "")
+#write.table(annotation, "./TnodPleMat_AnnotationTable_uniqueness2.txt", row.names = F, sep = "\t")
 
-write.table(annotation, "./TnodPleMat_AnnotationTable_uniqueness2.txt", row.names = F, sep = "\t")
-
-
-#one-to-one orthologs?
+## one-to-one orthologs for the tree!
+## present in each species
 presentinall <- tbl[complete.cases(tbl), ]
-#onetoone <- presentinall[presentinall$X..Species == presentinall$Genes, ] #too naive
-has.commas <- apply(presentinall, 1, function(x) grep(",", x))
-nocommas <- sapply(has.commas, function(x) length(x) == 0)
-onetoone <- presentinall[nocommas, ]
+## present in each species only once
+onetoone <- presentinall[presentinall$X..Species == presentinall$Genes, ] #too naive
+## 70 proteins as potential one-to-one orthologs
 
+## another way to check these calculations
+#has.commas <- apply(presentinall, 1, function(x) grep(",", x))
+#nocommas <- sapply(has.commas, function(x) length(x) == 0)
+#onetoone <- presentinall[nocommas, ]
+## 70 proteins as potential one-to-one orthologs
 
-#write name lists
+## write name lists to later extract
 for (i in 4:ncol(onetoone)) {
   writeLines(onetoone[,i], paste0(names(onetoone[i]), ".names.txt"))
 }
 
-# and families
+## and gene lists grouped by families
 for (i in 1:nrow(onetoone)) {
   oo <- as.matrix(onetoone)
   writeLines(oo[i, 4:21], paste0("family", as.character(i), ".names.txt"))
